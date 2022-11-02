cbec1c902b0fc6c7828ddb4e1249b7ee8e1f3aef
NoMcGowan <113493454+NoMcGowan@users.noreply.github.com>
2022-11-02 00:54:16 -0500

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -5,12 +5,16 @@ public class MainClass
 
 	public static void main(String[] args)
 	{
-		Rectangle r1 = new Rectangle(5, 2);
-		r1.draw();
-
-		Rectangle r2 = new Rectangle(8, 5);
-		r2.draw();
-
+		User user01 = new User("Saba", "Chicago");
+		user01.tweet("Hello everyone! This is my first tweet!").display();
+		
+		User user02 = new User("Joe");
+		user02.tweet("Hi! This is Joe!").display();
+		
+		User user03 = new User("Emily", "NYC");
+		user03.tweet("Such a beautiful day in New York today!").display();
+		
+		user02.tweet("I wish I could get some pizza right now!").display();
 	}
 
 }
